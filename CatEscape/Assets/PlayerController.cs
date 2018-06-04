9643c000396f7f2c9ba3781ffd030d1731f9ce16
kougei-223 <1727050@st.t-ac.jp>
2018-06-04 18:40:26 +0900

    最初のコミット

---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	void Start () {
+	}
+
+    public void LButtoDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtoDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);
+        }
+		
+	}
+}
